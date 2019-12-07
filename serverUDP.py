1b0d9856d17d4e03bdac528b52854a5751dc02b4
krojasalfaro8 <krojas.alfaro7@gmail.com>
2019-12-07 17:37:44 -0400

    Subiendo un nuevo archivo serverUDP.py y agregando información sobre instalación de sox

---
+++ serverUDP.py
@@ -0,0 +1,154 @@
+#================================================================================================
+#                                 Servidor UDP-Streaming
+#================================================================================================
+
+#==============
+# Bibliotecas
+#==============
+
+import socket  
+import wave, struct
+import pyaudio
+import os
+import threading
+import time
+from pydub import AudioSegment
+
+#=========================================================================
+# Funciones para convertir formatos de audio y envio - Programa Principal
+#=========================================================================
+
+def convertidor(nombre_archivo):                # Funcion que permite convertir un archivo .WAV 8Khz, 8bits a muestras PCM
+    global waveFile
+    global length
+    global frecuencia_1
+
+    nombre_archivo = nombre_archivo + "wav"
+    waveFile = wave.open(directorio + nombre_archivo, 'rb') # Se van a abrir los archivos que esten en esa carpeta
+
+    formato = p.get_format_from_width(waveFile.getsampwidth())
+    canales = waveFile.getnchannels()
+    frecuencia_1 = waveFile.getframerate()
+    frecuencia = int(waveFile.getframerate()/8000)
+
+    lista = str(formato) + '.' + str(canales) + '.' + str(frecuencia)
+
+    print(lista, frecuencia_1)
+    length = waveFile.getnframes()
+
+def revisar():                                              # Rutina que revisa si el archivo tiene extenxion .wav
+
+    global error
+    global audio
+    wav = int(7823734)
+    mensaje_r = mensaje                                     # Si no la tiene lo convierte, 
+    mensaje_l = mensaje
+    mensaje_r = mensaje_r[len(mensaje_r)-3:]                # Solo se queda con la extenxion 
+    mensaje_r = int.from_bytes(mensaje_r, byteorder = 'big')# Para convertir la cadena byte a int
+    lista_musica = os.listdir(directorio)
+
+    #print(lista_musica)
+    #print(mensaje.decode())
+    #print(lista_musica.index(mensaje.decode()))
+
+    try:
+        lista_musica.index(mensaje.decode())
+        try:
+
+            audio_revisar = mensaje.decode()[:len(mensaje)-3] + "wav"
+            lista_musica.index(audio_revisar)
+
+        except ValueError:
+
+            print('No se encuentra en formato .wav')
+
+            if (mensaje_r - wav) != 0:
+                mensaje_n = mensaje
+                mensaje_3 = mensaje[len(mensaje)-3:]
+
+                if mensaje_3 == b'mp3':
+                    print('Convirtiendo de formato .mp3 a formato .wav ...')
+                    sound_mp3 = AudioSegment.from_mp3(directorio + mensaje_n.decode())
+                    sound_mp3.export(directorio + mensaje_n.decode()[:len(mensaje)-3] + 'wav', format="wav")
+
+                elif mensaje_3 == b'ogg':
+                    print('Convirtiendo de formato .ogg a formato .wav ...')
+                    sound_ogg = AudioSegment.from_ogg(directorio + '\\' + mensaje_n.decode())
+                    sound_ogg.export(directorio + '\\' + mensaje_n.decode()[:len(mensaje)-3] + 'wav', format="wav")    
+
+                elif mensaje_3 == b'flv':
+                    print('Convirtiendo de formato .flv a formato .wav ...')
+                    sound_flv = AudioSegment.from_ogg(directorio + '\\' + mensaje_n.decode())
+                    sound_flv.export(directorio + '\\' + mensaje_n.decode()[:len(mensaje)-3] + 'wav', format="wav")    
+
+                elif mensaje_3 == b'aac':
+                    print('Convirtiendo de formato .aac a formato .wav ...')
+                    sound_aac = AudioSegment.from_file(directorio + '\\' + mensaje_n.decode(), 'aac')
+                    sound_aac.export(directorio + '\\' + mensaje_n.decode()[:len(mensaje)-3] + 'wav', format="wav")    
+
+    except ValueError:
+        print('no se encuentra el audio requerido')
+        error = b'error'
+           
+
+def enviar():
+
+    fragmento = length/1000
+    fragmento = int(fragmento)+ 1
+    for i in range(0, fragmento):
+        datos = waveFile.readframes(1000)
+        for i in range(0, len(direcciones)):
+            addr = (direcciones[i], 9999)
+            #print("Estos son los datos"+ int(datos[i]))
+            s.sendto(datos, addr)
+        if frecuencia_1 == 44100:
+            time.sleep(0.019666666999999999999)
+            #time.sleep(0.016666666999999999999)
+        else:
+            time.sleep(0.09999996942749023)
+
+#=============================================================================
+
+# Fin funciones para convertir formatos de audio y envio - Programa Principal
+
+#=============================================================================
+
+#====================================
+# Inicio del Main - Programa General
+#====================================
+
+if __name__ == '__main__':
+
+    global direcciones
+    global directorio
+
+    #direcciones =["192.168.1.10","192.168.1.11","192.168.1.12","192.168.1.13","192.168.1.14"]
+
+    direcciones =["192.168.1.7"]
+    directorio = '/home/kevin/audios/'
+
+    IP_UDP = socket.gethostbyname_ex(socket.gethostname())[2][0]
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Asignando socket para conexion UDP
+    s.bind(('',9999))
+
+    p = pyaudio.PyAudio()
+
+    mensaje=b'farewell.wav'
+
+    while True:                             # El servidor UDP se queda mandando constantemente.
+                
+        if mensaje == b'Funcion_1':
+            pass
+
+        else:                               # Revisa si la musica requerida esta en formato .wav
+            error = b'-'
+            revisar()                       # Si no lo esta lo convierte a ese formato y lo conveirte a PCM
+            if error != b'error':
+                audio = mensaje.decode()[:len(mensaje)-3] # Convierte el bytes en string
+                convertidor(audio)              # Convierte el audio .wav a codificacion PCM
+                enviar()                        # Envia los datos de audio al ESP8266
+            time.sleep(2)
+
+#=================================
+# Fin del Main - Programa General
+#=================================
